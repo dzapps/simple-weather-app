f1cc1dc314a6320e5f2c4fa85233722c9fff3c94
Vlad <vldvel@rambler.ru>
2017-04-18 13:49:06 +0300

    comment select2

--- js/components/weather.component/weather.component.js
+++ js/components/weather.component/weather.component.js
@@ -5,7 +5,7 @@ angular.module('weatherapp')
         var vm = this;
 
         // INIT SELECT2
-        $('select').select2({width: 'true'});
+        //$('select').select2({width: 'true'});
 
         vm.getWeather =  function(city) {
           $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + city + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
